1999f23638073df2eee19fa9d967d712ea37c062
AlvaroCubi <55387701+AlvaroCubi@users.noreply.github.com>
2025-08-07 09:29:59 +0200

    rssa plotter

--- src/f4enix/output/rssa.py
+++ src/f4enix/output/rssa.py
@@ -1,11 +1,13 @@
 """This module is related to the parsing of D1S-UNED meshinfo files."""
 
+from collections.abc import Sequence
 from dataclasses import dataclass
 from pathlib import Path
-from typing import BinaryIO, Sequence
+from typing import BinaryIO, Literal, Self
 
 import numpy as np
 import polars as pl
+from matplotlib import colors
 from matplotlib import pyplot as plt
 from matplotlib.axes import Axes
 from matplotlib.figure import Figure
@@ -141,46 +143,265 @@ class RSSA:
         """Returns the photon tracks from the RSSA file."""
         return self.tracks.filter(pl.col("b") != NEUTRON_INDICATOR)
 
-    def plot_tracks_on_cyl(self, bin_width: int | float = 10) -> None:
+    def plot_current_on_cyl(
+        self,
+        particle_type: Literal["n", "p"] = "n",
+        bin_width: float = 10.0,
+        surface_ids: list[int] | None = None,
+        source_intensity: float = 1.0,
+        z_limits: tuple[float, float] | None = None,
+        perimeter_limits: tuple[float, float] | None = None,
+    ) -> None:
         raise NotImplementedError
 
+    def plot_plane(self) -> "RSSAPlot":
+        """Returns an instance of RSSAPlot to plot the RSSA data assumin an XY plane."""
+        return RSSAPlot(self)
 
-def get_2d_grid_of_weights(
-    df: pl.DataFrame,
-    x_bins: Sequence[float] | pl.Series,
-    y_bins: Sequence[float] | pl.Series,
-    x_col: str = "x",
-    y_col: str = "y",
-) -> np.ndarray:
-    # Ensure bins are sorted for search_sorted function
-    x_bins = pl.Series("x_bins", x_bins).sort()
-    y_bins = pl.Series("y_bins", y_bins).sort()
+    def plot_cyl(self) -> "RSSAPlot":
+        """Returns an instance of RSSAPlotCyl to plot the RSSA data asuming a
+        cylindrical geometry with an axis following the Z-coordinate axis."""
+        return RSSAPlot(self, x_col="perimeter_pos", y_col="z")
 
-    # Remove points outside of the bins
-    filtered_df = (
-        df.lazy()
-        .select([x_col, y_col, "wgt"])
-        .filter(
-            pl.col(x_col).is_between(x_bins[0], x_bins[-1], closed="left"),
-            pl.col(y_col).is_between(y_bins[0], y_bins[-1], closed="left"),
+
+@dataclass
+class PlotParameters:
+    title: str = ""
+    xlabel: str = "X [cm]"
+    ylabel: str = "Y [cm]"
+    legend_label: str = "Particle current [#/cm2/s]"
+    legend_orientation: Literal["vertical", "horizontal"] = "horizontal"
+    number_of_colors: int = 10
+    norm: Literal["linear", "log"] = "log"
+    vmin: float | None = None
+    vmax: float | None = None
+
+
+class RSSAPlot:
+    def __init__(self, rssa: RSSA, x_col: str = "x", y_col: str = "y"):
+        self.tracks = rssa.tracks.lazy()
+        self.rssa_parameters = rssa.parameters
+        self.x_col = x_col
+        self.y_col = y_col
+        self._x_bins: pl.Series | None = None
+        self._y_bins: pl.Series | None = None
+        self.raster: np.ndarray | None = None
+        self.plot_parameters = PlotParameters()
+
+    @property
+    def x_bins(self) -> pl.Series:
+        """Returns the x bins for the plot."""
+        if self._x_bins is None:
+            raise ValueError("X bins are not set. Call set_bins() or calculate_bins().")
+        return self._x_bins
+
+    @property
+    def y_bins(self) -> pl.Series:
+        """Returns the y bins for the plot."""
+        if self._y_bins is None:
+            raise ValueError("Y bins are not set. Call set_bins() or calculate_bins().")
+        return self._y_bins
+
+    def set_particle(self, particle_type: Literal["n", "p"]) -> Self:
+        """Set the particle type to filter the tracks."""
+        if particle_type == "n":
+            self.tracks = self.tracks.filter(pl.col("b") == NEUTRON_INDICATOR)
+        elif particle_type == "p":
+            self.tracks = self.tracks.filter(pl.col("b") != NEUTRON_INDICATOR)
+        return self
+
+    def set_surface_ids(self, surface_ids: list[int]) -> Self:
+        """Set the surface IDs to filter the tracks."""
+        valid_surface_ids = [s.id for s in self.rssa_parameters.surfaces]
+        if not all(sid in valid_surface_ids for sid in surface_ids):
+            raise ValueError(
+                f"Some surface IDs are not valid. Valid IDs are: {valid_surface_ids}"
+            )
+        self.tracks = self.tracks.filter(pl.col("c").is_in(surface_ids))
+        return self
+
+    def set_z_limits(self, vmin: float, vmax: float) -> Self:
+        """Set the z limits for the plot."""
+        self.tracks = self.tracks.filter(
+            pl.col("z").is_between(vmin, vmax, closed="both")
         )
-        .collect()
-    )
-    grid = (
-        filtered_df.lazy()
-        # Find the bin indices for each point
-        .with_columns(
-            (x_bins.search_sorted(filtered_df[x_col], side="right") - 1).alias("bin_x"),
-            (y_bins.search_sorted(filtered_df[y_col], side="right") - 1).alias("bin_y"),
-        )
-        # Group by the bin indices and sum the weights
-        .group_by(["bin_x", "bin_y"])
-        .agg(pl.col("wgt").sum().alias("wgt"))
-        .collect()
-    )
-    raster = _get_raster(grid, x_bins, y_bins)
+        return self
 
-    return raster
+    def set_perimeter_limits(self, vmin: float, vmax: float) -> Self:
+        """Set the limits for the perimeter positions."""
+        if "perimeter_pos" not in self.tracks.collect_schema().names():
+            self.calculate_perimeter_positions()
+        self.tracks = self.tracks.filter(
+            pl.col("perimeter_pos").is_between(vmin, vmax, closed="both")
+        )
+        return self
+
+    def calculate_perimeter_positions(self) -> Self:
+        radius = (pl.col("x").pow(2) + pl.col("y").pow(2)).sqrt().mean()
+        thetas = pl.arctan2(pl.col("y"), pl.col("x"))
+        perimeter_pos = (thetas * radius).alias("perimeter_pos")
+        self.tracks = self.tracks.with_columns(perimeter_pos)
+        return self
+
+    def set_bins(self, x_bins: Sequence[float], y_bins: Sequence[float]) -> Self:
+        """Set the x and y bins for the plot."""
+        self._x_bins = pl.Series("x_bins", x_bins).sort()
+        self._y_bins = pl.Series("y_bins", y_bins).sort()
+        return self
+
+    def calculate_bins(self, bin_width: float = 10.0) -> Self:
+        """Automatically calculate the bins for the x and y coordinates of the plot by
+        giving a bin width in cm. Instead use `set_bins()` to apply custom bins."""
+        self._ensure_that_columns_are_set()
+        collected_tracks = self.tracks.collect()
+        if collected_tracks.is_empty():
+            raise ValueError("The tracks DataFrame is empty at this point.")
+        x_min = collected_tracks[self.x_col].min()
+        x_max = collected_tracks[self.x_col].max()
+        y_min = collected_tracks[self.y_col].min()
+        y_max = collected_tracks[self.y_col].max()
+        return self.set_bins(
+            np.arange(x_min, x_max + bin_width, bin_width),  # type: ignore
+            np.arange(y_min, y_max + bin_width, bin_width),  # type: ignore
+        )
+
+    def _ensure_that_columns_are_set(self) -> None:
+        if (
+            self.x_col == "perimeter_pos"
+            and self.x_col not in self.tracks.collect_schema().names()
+        ):
+            self.calculate_perimeter_positions()
+
+    def get_particle_current(self, source_intensity: float) -> Self:
+        """Calculate the particle current from the tracks. It automatically divides the
+        weight by the nps value."""
+        self.apply_source_intensity(source_intensity)
+        self.divide_by_nps()
+        raster = self._get_2d_grid_of_weights(agg_func="sum")
+        self.raster = raster / calculate_areas(self.x_bins, self.y_bins)
+        return self
+
+    def get_particle_current_errors(self) -> Self:
+        """Calculate the particle current errors from the tracks as the square root of
+        the number of tracks in each bin.
+        """
+        raster = self._get_2d_grid_of_weights(agg_func="count")
+        self.raster = raster**0.5
+        return self
+
+    def apply_source_intensity(self, source_intensity: float) -> Self:
+        """Apply a source intensity to the weights of the tracks."""
+        self.tracks = self.tracks.with_columns(
+            (pl.col("wgt") * source_intensity).alias("wgt")
+        )
+        return self
+
+    def divide_by_nps(self) -> Self:
+        """Divide the weights by the number of histories. The nps value used is the one
+        read in the header abs(np1)."""
+        self.tracks = self.tracks.with_columns(
+            (pl.col("wgt") / abs(self.rssa_parameters.np1)).alias("wgt")
+        )
+        return self
+
+    def set_plot_parameters(self, plot_parameters: PlotParameters) -> Self:
+        """Set the plot parameters for the plot."""
+        self.plot_parameters = plot_parameters
+        return self
+
+    def get_plot(self) -> tuple[Figure, Axes]:
+        if self.raster is None:
+            raise ValueError(
+                "Raster data is not calculated. Call get_particle_current()"
+                " or other first."
+            )
+        norm = (
+            colors.LogNorm if self.plot_parameters.norm == "log" else colors.Normalize
+        )
+
+        fig, ax = plt.subplots()
+        im = ax.pcolormesh(
+            self.x_bins.to_numpy(),
+            self.y_bins.to_numpy(),
+            self.raster,
+            cmap=plt.get_cmap("jet", self.plot_parameters.number_of_colors),
+            norm=norm(self.plot_parameters.vmin, self.plot_parameters.vmax),
+        )
+        fig.colorbar(
+            im,
+            ax=ax,
+            label=self.plot_parameters.legend_label,
+            orientation=self.plot_parameters.legend_orientation,
+        )
+        ax.set_aspect("equal")
+        ax.set_title(self.plot_parameters.title)
+        ax.set_xlabel(self.plot_parameters.xlabel)
+        ax.set_ylabel(self.plot_parameters.ylabel)
+        return fig, ax
+
+    def save_figure(self, out_path: Path | str) -> Self:
+        """Save the figure to the specified path."""
+        fig, ax = self.get_plot()
+
+        out_path = Path(out_path)
+        if out_path.suffix != ".png":
+            out_path = Path(out_path).with_suffix(".png")
+        plt.savefig(out_path, dpi=300, bbox_inches="tight")
+        return self
+
+    def show(self) -> Self:
+        """Show the plot."""
+        fig, ax = self.get_plot()
+        plt.show()
+        return self
+
+    def _get_2d_grid_of_weights(
+        self,
+        agg_func: Literal["sum", "count"] = "sum",
+    ) -> np.ndarray:
+        self._ensure_that_columns_are_set()
+
+        # Remove points outside of the bins
+        filtered_df = (
+            self.tracks.select([self.x_col, self.y_col, "wgt"])
+            .filter(
+                pl.col(self.x_col).is_between(
+                    self.x_bins[0],
+                    self.x_bins[-1],
+                    closed="left",
+                ),
+                pl.col(self.y_col).is_between(
+                    self.y_bins[0],
+                    self.y_bins[-1],
+                    closed="left",
+                ),
+            )
+            .collect()
+        )
+
+        # Decide if the weights are summed or the number of tracks are counted
+        agg_expression = (
+            pl.col("wgt").sum() if agg_func == "sum" else pl.col("wgt").count()
+        )
+        grid = (
+            filtered_df.lazy()
+            # Find the bin indices for each point
+            .with_columns(
+                (
+                    self.x_bins.search_sorted(filtered_df[self.x_col], side="right") - 1
+                ).alias("bin_x"),
+                (
+                    self.y_bins.search_sorted(filtered_df[self.y_col], side="right") - 1
+                ).alias("bin_y"),
+            )
+            # Group by the bin indices and sum the weights
+            .group_by(["bin_x", "bin_y"])
+            .agg(agg_expression)
+            .collect()
+        )
+        raster = _get_raster(grid, self.x_bins, self.y_bins)
+
+        return raster
 
 
 def _get_raster(
